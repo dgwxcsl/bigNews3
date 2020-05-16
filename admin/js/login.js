1d4953abcdb85c711ca0155d6289fc71193ad081
gobalDgwxcsl <2226292361@qq.com>
2020-05-16 18:56:52 +0800

    02-登录功能完成

---
+++ admin/js/login.js
@@ -0,0 +1,38 @@
+$(function () {
+    $('.login_form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'post',
+            url: BigNew.user_login,
+            data: $(this).serialize(),
+            beforeSend: function () {
+                var flag = false
+                $('.login_form input[name]').each(function (index, ele) {
+                    if (!$.trim($(ele).val())) {
+                        flag = true
+                    }
+                })
+                if (flag) {
+                    $('.modal-body').text('用户名或密码不能为空')
+                    $('#myModal').modal()
+                    return false
+                }
+
+            },
+            success: function (res) {
+                console.log(res);               
+                $('.modal-body').text(res.msg)
+                $('#myModal').modal()
+                if (res.code === 200) {
+                    $('.modal').on('hidden.bs.modal', function () {
+                        window.localStorage.setItem('token',res.token)
+                        window.location.href='./index.html'
+                    })
+
+                }
+
+            }
+
+        })
+    })
+})
