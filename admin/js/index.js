e659ff0aaf8d5b75d10f105d0f42ab568012160c
gobalDgwxcsl <2226292361@qq.com>
2020-05-16 19:03:45 +0800

    03-显示个人姓名和图片信息

---
+++ admin/js/index.js
@@ -0,0 +1,18 @@
+$(function () {
+    $.ajax({
+        type: 'get',
+        url: BigNew.user_info,
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+        success: function (res) {
+            // console.log(res);
+            if (res.code===200) {
+                $('.user_info span').html(`欢迎&nbsp;&nbsp;${res.data.nickname}`)
+                $('.user_info img').attr('src',res.data. userPic)
+                $('.user_center_link img').attr('src',res.data.userPic)
+            }
+        }
+
+    })
+})
